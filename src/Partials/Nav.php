c9c59516f40e2e9b4f5ce778a8259dfb68975b22
Peter Salvato <ptsalvato@gmail.com>
2025-04-17 15:29:33 +0000

    nav slug fix

--- src/Partials/Nav.php
+++ src/Partials/Nav.php
@@ -8,21 +8,21 @@
       <ul>
         <li><strong>Conceptual Projects</strong>
           <ul>
-            <li><a href="/index.php?page=workshop/conceptualProjects/photogeography">Photogeography</a></li>
-            <li><a href="/index.php?page=workshop/conceptualProjects/versograms">Versograms</a></li>
+            <li><a href="/index.php?page=workshop/conceptual-projects/photogeography">Photogeography</a></li>
+            <li><a href="/index.php?page=workshop/conceptual-projects/versograms">Versograms</a></li>
           </ul>
         </li>
         <li><strong>Case Studies</strong>
           <ul>
-            <li><a href="/index.php?page=workshop/caseStudies/aidenJae">Aiden Jae</a></li>
+            <li><a href="/index.php?page=workshop/case-studies/aiden-jae">Aiden Jae</a></li>
             <li><a href="/index.php?page=project&project=altrueism">Altrueism</a></li>
-            <li><a href="/index.php?page=workshop/caseStudies/mathOnTape">MathOnTape</a></li>
+            <li><a href="/index.php?page=workshop/case-studies/math-on-tape">MathOnTape</a></li>
           </ul>
         </li>
         <li><strong>Artifacts</strong>
           <ul>
-            <li><a href="/index.php?page=workshop/artifacts/theDeepCuts">The Deep Cuts</a></li>
-            <li><a href="/index.php?page=project&project=echoAndBone">Echo & Stone</a></li>
+            <li><a href="/index.php?page=workshop/artifacts/the-deep-cuts">The Deep Cuts</a></li>
+            <li><a href="/index.php?page=project&project=echo-and-bone">Echo & Stone</a></li>
           </ul>
         </li>
       </ul>
@@ -30,8 +30,8 @@
     <li>
       <span>Systems</span>
       <ul>
-        <li><a href="/index.php?page=systems/savepointProtocol">Savepoint Protocol</a></li>
-        <li><a href="/index.php?page=systems/theOrderOfTheAetherwright">The Order of<br><span>the Ætherwright</span></a></li>
+        <li><a href="/index.php?page=systems/savepoint-protocol">Savepoint Protocol</a></li>
+        <li><a href="/index.php?page=systems/the-order-of-the-aetherwright">The Order of<br><span>the Ætherwright</span></a></li>
       </ul>
     </li>
     <li><a href="/index.php?page=journal">Journal</a></li>
